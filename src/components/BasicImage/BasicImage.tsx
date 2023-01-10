63ebdc5c4d8367a94c60a28ef25fdbbb0da0ceda
anna.pulis <anna.pulis@credera.com>
2023-01-10 15:15:18 -0600

    Made props optional after talking to Tiffany

--- src/components/BasicImage/BasicImage.tsx
+++ src/components/BasicImage/BasicImage.tsx
@@ -2,14 +2,14 @@ import React from "react";
 import "./BasicImage.scss";
 
 export interface BasicImageProps {
-  image: string;
+  image?: string;
   imageAltText?: string;
 }
 
 const BasicImage = (props: BasicImageProps) => {
   return (
     <div className="basic-image">
-      <img src={props.image} alt={props.imageAltText} title={props.imageAltText} />
+      {props.image ? <img src={props.image} alt={props.imageAltText} title={props.imageAltText} /> : ''}
     </div>
   );
 };
