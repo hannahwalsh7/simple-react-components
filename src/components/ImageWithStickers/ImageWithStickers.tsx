98882abd8eab153adc45b10682d6b8e8f538f278
anna-pulis-cfa <anna.pulis1@accesscfa.com>
2023-01-13 11:40:48 -0600

    Created Page Header, updated image with stickers

--- src/components/ImageWithStickers/ImageWithStickers.tsx
+++ src/components/ImageWithStickers/ImageWithStickers.tsx
@@ -2,23 +2,53 @@ import React from "react";
 import "./ImageWithStickers.scss";
 import TopSticker from "../TopSticker";
 import BottomSticker from "../BottomSticker";
+import BasicImage from "../BasicImage";
 
 export interface ImageWithStickersProps {
-  imageSource?: string;
+  image?: string;
+  imageAltText?: string;
+  imageOptions?: string;
   topStickerName?: string;
   topStickerColor?: string;
   bottomStickerName?: string;
   bottomStickerColor?: string;
-  removeTopSticker?: boolean;
-  removeBottomSticker?: boolean;
 }
 
 const ImageWithStickers = (props: ImageWithStickersProps) => {
+  let removeImage: boolean = false;
+  let removeTopSticker: boolean = false;
+  let removeBottomSticker: boolean = false;
+  
+  if (props.imageOptions === "topSticker") {
+    removeImage = false;
+    removeTopSticker = false;
+    removeBottomSticker = true;
+  }
+
+  if (props.imageOptions === "bottomSticker") {
+    removeImage = false;
+    removeTopSticker = true;
+    removeBottomSticker = false;
+  }
+
+  if (props.imageOptions === "noStickers") {
+    removeImage = false;
+    removeTopSticker = true;
+    removeBottomSticker = true;
+  }
+
+  if (props.imageOptions === "none") {
+    removeImage = true;
+    removeTopSticker = true;
+    removeBottomSticker = true;
+  }
+
+ 
   return (
-    <div className={`image-with-stickers ${props.removeTopSticker ? `image-with-stickers--remove-top-sticker` : ''} ${props.removeBottomSticker ? `image-with-stickers--remove-bottom-sticker` : ''}`}>
-      <img src={props.imageSource} />
-      {props.removeTopSticker ? "" : <TopSticker iconName={props.topStickerName} color={props.topStickerColor} />}
-      {props.removeBottomSticker ? "" : <BottomSticker iconName={props.bottomStickerName} color={props.bottomStickerColor} />}
+    <div className={`image-with-stickers ${removeImage ? `image-with-stickers--remove-image` : ''} ${removeTopSticker ? `image-with-stickers--remove-top-sticker` : ''} ${removeBottomSticker ? `image-with-stickers--remove-bottom-sticker` : ''}`}>
+      {removeImage ? "" : <BasicImage image={props.image} imageAltText={props.imageAltText} />}
+      {removeTopSticker ? "" : <TopSticker iconName={props.topStickerName} color={props.topStickerColor} />}
+      {removeBottomSticker ? "" : <BottomSticker iconName={props.bottomStickerName} color={props.bottomStickerColor} />}
     </div>
   );
 };
--- src/components/PageHeaderNew/PageHeaderNew.stories.tsx
+++ src/components/PageHeaderNew/PageHeaderNew.stories.tsx
@@ -1,20 +1,30 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
-import ImageWithStickers from './ImageWithStickers';
+import PageHeaderNew from './PageHeaderNew';
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-  title: 'React Component Library/Image with Stickers',
-  component: ImageWithStickers,
-} as ComponentMeta<typeof ImageWithStickers>;
+  title: 'React Component Library/Page Header New',
+  component: PageHeaderNew,
+} as ComponentMeta<typeof PageHeaderNew>;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template: ComponentStory<typeof ImageWithStickers> = (args) => <ImageWithStickers {...args} />;
+const Template: ComponentStory<typeof PageHeaderNew> = (args) => <PageHeaderNew {...args} />;
 
-export const ImageWithStickersStory = Template.bind({});
+export const PageHeaderNewStory = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
-ImageWithStickersStory.argTypes = {
+PageHeaderNewStory.argTypes = {
+  title: {
+    name: 'Title',
+    control: { type: 'text' },
+    description: 'Banner title',
+  },
+  subtitle: {
+    name: 'Subtitle',
+    control: { type: 'text' },
+    description: 'Banner subtitle',
+  },
   image: {
     name: 'Image',
     control: { type: 'text' },
@@ -49,7 +59,7 @@ ImageWithStickersStory.argTypes = {
       category: 'Image',
     },
   },
-  topStickerName: {
+  topSticker: {
     name: 'Icon Name',
     description: 'Name of icon',
     options: [
@@ -81,7 +91,7 @@ ImageWithStickersStory.argTypes = {
       category: 'Top Sticker',
     }
   },
-  bottomStickerName: {
+  bottomSticker: {
     name: 'Icon Name',
     description: 'Name of icon',
     options: [
@@ -116,14 +126,16 @@ ImageWithStickersStory.argTypes = {
     }
   }
 }
-ImageWithStickersStory.args = {
+PageHeaderNewStory.args = {
+  title: "We're here for one another.",
+  subtitle: "We are a team-obsessed and customer-focused organization.",
   image: "https://images.unsplash.com/photo-1508184585643-6001cf47a1ca?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80",
   imageAltText: "Trees and mountains",
   imageOptions: "allStickers",
-  topStickerName: "cfa-tagline",
+  topSticker: "cfa-tagline",
   topStickerColor: "1",
-  bottomStickerName: "occasions_collaboration-2",
+  bottomSticker: "occasions_collaboration-2",
   bottomStickerColor: "1",
 };
 
-ImageWithStickersStory.storyName = "Image with Stickers";
+PageHeaderNewStory.storyName = "Page Header New";
