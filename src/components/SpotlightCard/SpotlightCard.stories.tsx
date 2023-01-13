b7f4e7a5cd044cc0a8459d5948a613c98818ee62
Mrityunjay Mishra <mrityunjay.mishra@credera.com>
2023-01-13 11:41:29 -0600

    Added content and styling+logic for it

--- src/components/SpotlightCard/SpotlightCard.stories.tsx
+++ src/components/SpotlightCard/SpotlightCard.stories.tsx
@@ -58,12 +58,14 @@ SpotlightCardStory.argTypes = {
     }
   }
 };
+
+const description = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
 SpotlightCardStory.args = {
     title: 'Test Title',
     imageSource: 'https://icons.veryicon.com/png/o/internet--web/55-common-web-icons/person-4.png',
     imageAltText: 'Test alt text',
     subtitle: 'Test subtitle',
-    description: 'Test description',
+    description: description,
     textLink: 'Test link'
 };
 
--- src/components/SpotlightCard/SpotlightCard.tsx
+++ src/components/SpotlightCard/SpotlightCard.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./SpotlightCard.scss";
 import TextLink from "../TextLink";
 
@@ -13,6 +13,41 @@ export interface SpotlightCardProps {
 }
 
 const SpotlightCard = (props: SpotlightCardProps) => {
+    const [expanded, setExpanded] = useState(false);
+    const [description, setDescription] =  useState('');
+    const [showReadToggle, setShowReadToggle] = useState(false);
+    const [readToggleText, setReadToggleText] = useState('');
+
+    const textLinkText = 'Meet the Leadership Team';
+    const substringLength = 250;
+
+    useEffect(() => {
+        if(props.description !== undefined){
+            let description = '';
+            let showReadToggle = false;
+            let readToggleText = '';
+
+            if(expanded) {
+                description = props.description;
+                if(props.description.length > substringLength){
+                    showReadToggle = true;
+                    readToggleText = 'Read Less';
+                }
+            } else {
+                description = props.description.substring(0,substringLength);
+                if(props.description.length > substringLength){
+                    description += '...';
+                    showReadToggle = true;
+                    readToggleText = 'Read More';
+                }
+            }
+
+            setDescription(description);
+            setShowReadToggle(showReadToggle);
+            setReadToggleText(readToggleText);
+        }
+    }, [expanded])
+
     return (
       <div className='spotlight-card mt mb'>
         <div className='spotlight-card__img-container'>
@@ -26,10 +61,17 @@ const SpotlightCard = (props: SpotlightCardProps) => {
                 <h2 className="spotlight-card__content">
                     {props.subtitle}
                 </h2>
-                <p className="spotlight-card__content">
-                    {props.description}
-                </p>
+                {props.description !== undefined ? 
+                    <>
+                        <p className="spotlight-card__content">
+                            {description} {showReadToggle ? <a onClick={() => setExpanded(!expanded)}>{readToggleText}</a> : null }
+                        </p> 
+                        <br/>
+                    </>
+                :null}
+                {props.textLink ? <TextLink linkUrl={props.textLink} linkText={textLinkText}/> : null}
             </div>
+            
 
             
         </div>
